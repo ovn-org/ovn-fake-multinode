92c636412533ea3b929014aa2816584b32f18475
Flavio Fernandes <flaviof@redhat.com>
2020-01-30 16:54:13 -0500

    install_docker.sh: Add vagrant user to docker group
    While provisioning docker (via Vagrant), include vagrant user as
    part of the docker group.

--- provisioning/install_docker.sh
+++ provisioning/install_docker.sh
@@ -16,5 +16,7 @@ else
     dnf install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 fi
 
+[ -d /home/vagrant ] && usermod -a -G docker vagrant
+
 systemctl disable firewalld   ;  # yuck!
 systemctl enable --now docker
