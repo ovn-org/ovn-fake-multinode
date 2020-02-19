ovn-org/ovn-fake-multinode

commit c841715625f927ec1dfaebbe82671b31b326a699

Add option to enable OVS/OVN debug builds.
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

fedora/ovn/Dockerfile
@@ -4,6 +4,7 @@ MAINTAINER "Numan Siddique" <numans@ovn.org>
 ARG OVS_SRC_PATH
 ARG OVN_SRC_PATH
 ARG USE_OVN_RPMS
+ARG DEBUG_SYMS
 
 COPY $OVS_SRC_PATH /ovs
 COPY $OVN_SRC_PATH /ovn
@@ -11,7 +12,7 @@ COPY $OVN_SRC_PATH /ovn
 COPY *.rpm /
 COPY install_ovn.sh /install_ovn.sh
 
-RUN /install_ovn.sh $USE_OVN_RPMS
+RUN /install_ovn.sh $USE_OVN_RPMS $DEBUG_SYMS
 
 RUN dnf -y remove automake make gcc autoconf openssl-devel libtool
 

install_ovn.sh
@@ -16,7 +16,13 @@ set -o xtrace
 set -o errexit
 
 use_ovn_rpm=$1
-#ls ovn*.rpm > /dev/null || build_from_src="yes"
+debug_syms=$2
+
+if [ "$debug_syms" = "yes" ]; then
+    cflags='-g'
+else
+    cflags=
+fi
 
 if [ "$use_ovn_rpm" = "yes" ]; then
     ls ovn*.rpm > /dev/null || exit 1
@@ -27,7 +33,7 @@ else
     # build and install
     ./boot.sh
     ./configure --localstatedir="/var" --sysconfdir="/etc" --prefix="/usr" \
-    --enable-ssl --disable-libcapng
+    --enable-ssl --disable-libcapng CFLAGS="${cflags}"
     make -j$(($(nproc) + 1)) V=0
     make install
 
@@ -35,7 +41,8 @@ else
     # build and install
     ./boot.sh
     ./configure --localstatedir="/var" --sysconfdir="/etc" --prefix="/usr" \
-    --enable-ssl --with-ovs-source=/ovs/ --with-ovs-build=/ovs/
+    --enable-ssl --with-ovs-source=/ovs/ --with-ovs-build=/ovs/ \
+    CFLAGS="${cflags}"
     make -j$(($(nproc) + 1)) V=0
     make install
 fi

ovn_cluster.sh
@@ -11,6 +11,7 @@ CHASSIS_IMAGE="ovn/ovn-multi-node"
 GW_IMAGE="ovn/ovn-multi-node"
 
 USE_OVN_RPMS="${USE_OVN_RPMS:-no}"
+DEBUG_SYMS="${DEBUG_SYMS:-no}"
 
 CENTRAL_NAME="ovn-central"
 CHASSIS_PREFIX="${CHASSIS_PREFIX:-ovn-chassis-}"
@@ -584,7 +585,9 @@ function build-images() {
     ${RUNC_CMD} build -t ovn/cinc -f fedora/cinc/Dockerfile .
 
     ${RUNC_CMD} build -t ovn/ovn-multi-node --build-arg OVS_SRC_PATH=ovs \
-    --build-arg OVN_SRC_PATH=ovn --build-arg USE_OVN_RPMS=$USE_OVN_RPMS -f  fedora/ovn/Dockerfile .
+    --build-arg OVN_SRC_PATH=ovn --build-arg USE_OVN_RPMS=${USE_OVN_RPMS} \
+    --build-arg DEBUG_SYMS=${DEBUG_SYMS} \
+    -f  fedora/ovn/Dockerfile .
 }
 
 function check-for-ovn-rpms() {

commit 743b39a3c038fccaef844fb0e354c2d94309a848

Fix default compiler optimization flags.
Also add option to enable ultra optimized builds through the
EXTRA_OPTIMIZE env variable.

Fixes: c841715625f9 ("Add option to enable OVS/OVN debug builds.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

fedora/ovn/Dockerfile
@@ -4,7 +4,7 @@ MAINTAINER "Numan Siddique" <numans@ovn.org>
 ARG OVS_SRC_PATH
 ARG OVN_SRC_PATH
 ARG USE_OVN_RPMS
-ARG DEBUG_SYMS
+ARG EXTRA_OPTIMIZE
 
 COPY $OVS_SRC_PATH /ovs
 COPY $OVN_SRC_PATH /ovn
@@ -12,7 +12,7 @@ COPY $OVN_SRC_PATH /ovn
 COPY *.rpm /
 COPY install_ovn.sh /install_ovn.sh
 
-RUN /install_ovn.sh $USE_OVN_RPMS $DEBUG_SYMS
+RUN /install_ovn.sh $USE_OVN_RPMS $EXTRA_OPTIMIZE
 
 RUN dnf -y remove automake make gcc autoconf openssl-devel libtool
 

install_ovn.sh
@@ -16,12 +16,12 @@ set -o xtrace
 set -o errexit
 
 use_ovn_rpm=$1
-debug_syms=$2
+extra_optimize=$2
 
-if [ "$debug_syms" = "yes" ]; then
-    cflags='-g'
+if [ "$extra_optimize" = "yes" ]; then
+    cflags='-g -march=native -O3 -fno-omit-frame-pointer'
 else
-    cflags=
+    cflags='-g -O2 -fno-omit-frame-pointer'
 fi
 
 if [ "$use_ovn_rpm" = "yes" ]; then

ovn_cluster.sh
@@ -11,7 +11,7 @@ CHASSIS_IMAGE="ovn/ovn-multi-node"
 GW_IMAGE="ovn/ovn-multi-node"
 
 USE_OVN_RPMS="${USE_OVN_RPMS:-no}"
-DEBUG_SYMS="${DEBUG_SYMS:-no}"
+EXTRA_OPTIMIZE="${EXTRA_OPTIMIZE:-no}"
 
 CENTRAL_NAME="ovn-central"
 CHASSIS_PREFIX="${CHASSIS_PREFIX:-ovn-chassis-}"
@@ -596,7 +596,7 @@ function build-images() {
 
     ${RUNC_CMD} build -t ovn/ovn-multi-node --build-arg OVS_SRC_PATH=ovs \
     --build-arg OVN_SRC_PATH=ovn --build-arg USE_OVN_RPMS=${USE_OVN_RPMS} \
-    --build-arg DEBUG_SYMS=${DEBUG_SYMS} \
+    --build-arg EXTRA_OPTIMIZE=${EXTRA_OPTIMIZE} \
     -f  fedora/ovn/Dockerfile .
 }
 