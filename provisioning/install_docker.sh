5868636b447ac863b763408d36ce4ccb49c82d10
Flavio Fernandes <flaviof@redhat.com>
2020-12-09 16:36:42 -0500

    Vagrant: fix nesting and docker install
    Fix enable_nesting.sh so it works on newer kernels.
    Remove work around in install_docker.sh (no longer needed)

--- provisioning/install_docker.sh
+++ provisioning/install_docker.sh
@@ -7,14 +7,7 @@ set -o errexit
 
 # Ref: https://linuxconfig.org/how-to-install-docker-in-rhel-8
 dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-
-# We can get docker to work on Centos 8 in of these 2 ways
-if false; then
-    dnf install -y --nobest docker-ce
-else
-    dnf install -y docker-ce-3:18.09.1-3.el7
-    dnf install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
-fi
+dnf install -y docker-ce
 
 [ -d /home/vagrant ] && usermod -a -G docker vagrant
 
