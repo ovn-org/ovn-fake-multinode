630145022ca51044221bc0c118605f3e99f5b8df
Ales Musil <amusil@redhat.com>
2022-04-07 11:44:57 +0200

    Refactor the install_pkg script
    Unify the packages that needs to be installed,
    the Fedora and el8 should use the same packages
    even though the version might not be the same.
    
    Remove the CentOS repos as they are not
    functional, but the whole multinode for ub8
    still works.
    
    Last part of this refactor is to getting
    custom compilation of iputils, which is
    not required anymore and the iputils from
    rpm will suffice.
    
    Signed-off-by: Ales Musil <amusil@redhat.com>

--- fedora/cinc/install_pkg.sh
+++ fedora/cinc/install_pkg.sh
@@ -1,100 +1,56 @@
-
-image_name=${1:-"fedora:35"}
+#!/bin/bash -xe
 
 dnf -y install systemd
 
 systemctl mask \
-	auditd.service\
-	console-getty.service\
-	dev-hugepages.mount\
-	dnf-makecache.service\
-	getty.target\
-	lvm2-lvmetad.service\
-	sys-fs-fuse-connections.mount\
-	systemd-logind.service\
-	systemd-remount-fs.service\
-	systemd-udev-hwdb-update.service\
-	systemd-udevd.service\
+	auditd.service \
+	console-getty.service \
+	dev-hugepages.mount \
+	dnf-makecache.service \
+	getty.target \
+	lvm2-lvmetad.service \
+	sys-fs-fuse-connections.mount \
+	systemd-logind.service \
+	systemd-remount-fs.service \
+	systemd-udev-hwdb-update.service \
+	systemd-udevd.service \
 	systemd-vconsole-setup.service
 
-
-if echo $image_name | grep ubi8
-then
-
-	# add needed repo
-
-	cat > /etc/yum.repos.d/centos-appstream.repo <<EOF
-[centos-appstream]
-name=centos-appstream
-baseurl=http://mirror.centos.org/centos/8/AppStream/x86_64/os/
-enabled=1
-gpgcheck=0
-skip_if_unavailable=1
-EOF
-
-	cat > /etc/yum.repos.d/centos-baseos.repo <<EOF
-[centos-baseos]
-name=centos-baseos
-baseurl=http://mirror.centos.org/centos/8/BaseOS/x86_64/os/
-enabled=1
-gpgcheck=0
-skip_if_unavailable=1
-EOF
-
-	cat > /etc/yum.repos.d/centos-powertools.repo <<EOF
-[centos-powertools]
-name=centos-powertools
-baseurl=http://mirror.centos.org/centos/8/PowerTools/x86_64/os/
-enabled=1
-gpgcheck=0
-skip_if_unavailable=1
-EOF
-
-
-	dnf -y --skip-broken install automake make gcc autoconf openssl-devel \
-		python3 libtool openssl python3-pip \
-		net-tools.x86_64 uuid.x86_64 iproute.x86_64 dnf-utils libreswan \
-		conntrack-tools nmap ninja-build meson libcap-devel gettext-devel \
-		libxslt git iproute procps-ng
-	dnf remove -y iputils
-
-	git clone https://github.com/iputils/iputils.git
-	pushd iputils
-	./configure && make && make install
-	popd
-
-else
-	systemctl mask docker-storage-setup.service
-	dnf install docker pacemaker pcs -y --skip-broken
-	systemctl enable docker.service
-	systemctl enable pcsd.service
-
-	dnf -y --skip-broken install automake make gcc autoconf openssl-devel \
-		python3 libtool openssl python3-pip \
-		net-tools.x86_64 uuid.x86_64 iproute.x86_64 dnf-utils libreswan \
-		conntrack-tools nmap iputils which dhclient
-
-	# Default storage to vfs.  overlay will be enabled at runtime if available
-	echo "DOCKER_STORAGE_OPTIONS=--storage-driver vfs" > /etc/sysconfig/docker-storage
-	# Generate variation of dhclient-script that we can use for fake vm namespaces
-	mkdir -pv /bin
-	/tmp/generate_dhclient_script_for_fullstack.sh /
-fi
-
-dnf -y --skip-broken install\
-	glibc-langpack-en\
-	iptables\
-	openssh-clients\
-	openssh-server\
-	resource-agents\
-	tcpdump\
-	dhclient\
-	which\
-	fping\
-	perf\
-	git\
-	file\
-	lksctp-tools-devel\
-	hostname
-
-
+dnf -y install \
+  autoconf \
+  automake \
+  conntrack-tools \
+  dhcp-client \
+  dnf-utils \
+  file \
+  fping \
+  gcc \
+  gettext-devel \
+  git \
+  glibc-langpack-en \
+  hostname \
+  iproute \
+  iproute.x86_64 \
+  iptables \
+  libcap-devel \
+  libreswan \
+  libtool \
+  libxslt \
+  lksctp-tools-devel \
+  make \
+  meson \
+  net-tools.x86_64 \
+  ninja-build \
+  nmap \
+  openssh-clients \
+  openssh-server \
+  openssl \
+  openssl-devel \
+  perf \
+  procps-ng \
+  python3 \
+  python3-pip \
+  resource-agents \
+  tcpdump \
+  uuid.x86_64 \
+  which
